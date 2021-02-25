1e21c8d1f49b0102f53645f51b4f677ee3d10e81
kubotaissei <issei.k0504@me.com>
2021-02-25 14:49:52 +0900

    rebuilding site 2021年 2月25日 木曜日 14時49分52秒 JST

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ sed -i -e 's$official/official$official$g' docs/index.html
 rm docs/index.html-e
 sed -i -e 's$official/official$official$g' docs/posts/index.html
 rm docs/posts/index.html-e
+sed -i -e 's$official/official$official$g' docs/posts/blog001/index.html
+rm docs/posts/blog001/index.html-e
+sed -i -e 's$official/official$official$g' docs/posts/blog002/index.html
+rm docs/posts/blog002/index.html-e
 git add -A
 git commit -m "rebuilding site $(date)"
 git push -u origin master
